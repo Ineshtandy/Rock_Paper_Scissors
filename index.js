189563e4309637fc3482c09697061afff7a666aa
Inesh <i08polymath@gmail.com>
2022-07-17 15:15:20 +0530

    JAVASCRIPT UPDATE
    added more conditional blocks for all comparirisions
    added event listeners to paper and scissor buttons

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ let manCount = 0;
 let compCount = 0;
 let displayVal = document.querySelector("#display"); 
 let rockEl = document.querySelector("#rock-el");
-rockEl.addEventListener("click",game)
+let paperEl = document.querySelector("#paper-el");
+let scissorEl = document.querySelector("#scissor-el");
+
+rockEl.addEventListener("click",game);
+paperEl.addEventListener("click",game);
+scissorEl.addEventListener("click",game);
+
 
 
 function computerPlay() {
@@ -10,7 +16,7 @@ function computerPlay() {
     //console.log(ch);
     if(ch === 0)    return "rock";
     else if(ch === 1 )  return "paper";
-    else    return "scissors";
+    else return "scissor";
     //0 = rock
     //1 = paper
     //2 = scissors
@@ -18,14 +24,38 @@ function computerPlay() {
 
 
 function playRound(playerSelection, computerSelection) {
-    if(playerSelection.toUpperCase() === computerSelection.toUpperCase()){
-        return "draw";
-    }else if(computerSelection.toUpperCase() === "PAPER"){
-        compCount++;
-        return "You Lose! Paper beats Rock"
-    }else if(computerSelection.toUpperCase() === "SCISSORS"){
-        manCount++;
-        return "You Win! Rock beats Scissors"
+    // if(playerSelection.toUpperCase() === computerSelection.toUpperCase()){
+    //     return "draw";
+    // }else if(computerSelection.toUpperCase() === "PAPER"){
+    //     compCount++;
+    //     return "You Lose! Paper beats Rock"
+    // }else if(computerSelection.toUpperCase() === "SCISSORS"){
+    //     manCount++;
+    //     return "You Win! Rock beats Scissors"
+    // }
+    let P = playerSelection.toUpperCase();
+    let C = computerSelection.toUpperCase();
+    if(P === C){ return "draw"};
+    if(P === "ROCK"){
+        if(C === "PAPER"){
+            return "You Lose, Paper Beats Rock";
+        }else if(C === "SCISSORS"){
+            return "You Win, Rock Beats Scissors";
+        }
+    }
+    if(P === "PAPER"){
+        if(C === "ROCK"){
+            return "You win, Paper Beats Rock";
+        }else if(C === "SCISSORS"){
+            return "You Lose, Scissors Beats Paper";
+        }
+    }
+    if(P === "SCISSOR"){
+        if(C === "PAPER"){
+            return "You Win, Scissors Beats Paper";
+        }else if(C === "ROCK"){
+            return "You Lose, Rock Beats Scissors";
+        }
     }
 }
 
